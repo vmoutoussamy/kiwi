f641d52477636afe1daab02b01b89751292bcedb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-22 21:59:38 +0000

    Deploy SUSE/kiwi to github.com/SUSE/kiwi.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '9.17.37',
+    VERSION: '9.17.38',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
