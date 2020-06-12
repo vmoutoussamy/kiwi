6952218c082be80df892f54d63e81f77670830d7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-12 12:31:45 +0000

    Deploy OSInside/kiwi to github.com/OSInside/kiwi.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '9.21.0',
+    VERSION: '9.21.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
